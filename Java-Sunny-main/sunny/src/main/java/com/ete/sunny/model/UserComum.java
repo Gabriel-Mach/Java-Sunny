fd7e32cc4d1c744518d31416089d7df3d300d4de
Gabriel-Mach <59069859+Gabriel-Mach@users.noreply.github.com>
2023-05-04 21:13:18 -0300

    Add files via upload

---
+++ Java-Sunny-main/sunny/src/main/java/com/ete/sunny/model/UserComum.java
@@ -0,0 +1,31 @@
+package com.ete.sunny.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserComum extends Usuario{
+    @Column(nullable = false)
+    private int pontuacao;
+
+    @ManyToOne
+    @JoinColumn(name = "reponsavel")
+    private UserResponsavel responsavel;
+
+    @ManyToMany
+    @JoinTable(name = "usuario_tema",
+    joinColumns = @JoinColumn (name = "CPF"),
+            inverseJoinColumns = @JoinColumn(name = "titulo")
+    )
+    private List <Tema> temas;
+
+}
---
+++ Java-Sunny-main/sunny/src/main/java/com/ete/sunny/model/UserResponsavel.java
@@ -0,0 +1,26 @@
+package com.ete.sunny.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserResponsavel extends Usuario {
+    @Column(nullable = false)
+    private String telefone;
+
+
+    @OneToMany (mappedBy = "responsavel")
+    private List <UserComum> aluno;
+}
