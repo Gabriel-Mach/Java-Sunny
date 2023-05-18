bbc63aa2e26bee6cec48292dddae29db8f8fbb72
Gabriel-Mach <59069859+Gabriel-Mach@users.noreply.github.com>
2023-05-17 21:03:35 -0300

    Delete sunny directory

--- sunny/src/main/java/com/ete/sunny/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ete.sunny.repository;
-
-import com.ete.sunny.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,String> {
-    Usuario findByEmail(String email);
-
-}
